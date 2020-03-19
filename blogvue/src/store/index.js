b37291c490bdcfad583732f13b2e6fdae278ecaa
wyg <578292549@qq.com>
2020-03-19 17:54:41 +0800

    页面传值及Vuex

--- blogvue/src/store/index.js
+++ blogvue/src/store/index.js
@@ -4,9 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  //初始化数据
   state: {
+    formDatas:null   //定义一个变量  formDatas
   },
+  //改变state里面的值得方法
   mutations: {
+    getFormData(state,data)
+    {
+      state.formDatas=data;
+    }
   },
   actions: {
   },
